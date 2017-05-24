1b8286c6a96759087309b9d1a7d274d8e986d937
saidmattar <ssabdou@gmail.com>
2017-05-24 15:59:39 -0700

    added a heroku Postgres Database

--- server.js
+++ server.js
@@ -7,7 +7,7 @@ const bodyParser = require('body-parser');
 const requestProxy = require('express-request-proxy'); // REVIEW: We've added a new package here to our requirements, as well as in the package.json
 const PORT = process.env.PORT || 3000;
 const app = express();
-const conString = 'postgres://postgres:2468@localhost:5432/postgres';
+const conString =  process.env.DATABASE_URL || 'postgres://postgres:2468@localhost:5432/postgres';
 //const conString = ''; // DONE: Don't forget to set your own conString
 const client = new pg.Client(conString);
 client.connect();
